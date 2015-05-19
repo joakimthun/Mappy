d751c95ea8d483a2e545b7367d644a4099505405
Joakim Thun <joakimthun@gmail.com>
2015-05-19 07:29:37 +0200

    Added some more il stuff

--- Application/Program.cs
+++ Application/Program.cs
@@ -3,6 +3,7 @@ using Mappy.LazyLoading;
 using Mappy.Queries;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Application
 {
--- Mappy/LazyLoading/ProxyFactory.cs
+++ Mappy/LazyLoading/ProxyFactory.cs
@@ -91,11 +91,80 @@ namespace Mappy.LazyLoading
         {
             var property = DefineProperty(typeBuilder, propertyInfo);
             var field = DefineField(typeBuilder, propertyInfo);
-            var getter = DefineGetter(typeBuilder, propertyInfo, field);
+            //var getter = DefineGetter(typeBuilder, propertyInfo, field);
+
+            var test = DefineGetterTest(typeBuilder, propertyInfo, field);
+
             var setter = DefineSetter(typeBuilder, propertyInfo, field);
 
-            property.SetGetMethod(getter);
+            property.SetGetMethod(test);
             property.SetSetMethod(setter);
+        }
+
+        private MethodBuilder DefineGetterTest(TypeBuilder typeBuilder, PropertyInfo propertyInfo, FieldBuilder field)
+        {
+            var getter = typeBuilder.DefineMethod(string.Format("get_{0}", propertyInfo.Name), MethodAttributes.Virtual | MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, propertyInfo.PropertyType, Type.EmptyTypes);
+            var ilGenerator = getter.GetILGenerator();
+
+            Type propertyType;
+
+            if (propertyInfo.IsICollection())
+            {
+                propertyType = typeof(List<>).MakeGenericType(propertyInfo.GetUnderlyingPropertyType());
+            }
+            else
+            {
+                propertyType = propertyInfo.PropertyType;
+            }
+
+            var local = ilGenerator.DeclareLocal(propertyType);
+            var constructorInfo = propertyType.GetConstructor(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[0], null);
+
+            ilGenerator.Emit(OpCodes.Newobj, constructorInfo);
+            ilGenerator.Emit(OpCodes.Stloc, local);
+
+            ilGenerator.Emit(OpCodes.Ldloc, local);
+
+            ilGenerator.Emit(OpCodes.Ret);
+
+            //.method private hidebysig static class [mscorlib] System.Collections.Generic.ICollection`1<class Application.Post> Test(string[] args) cil managed
+            //{
+            //// Code size       33 (0x21)
+            //.maxstack  2
+            //.locals init ([0] class Application.BlogContext context,
+            //         [1] class [mscorlib]
+            //      System.Collections.Generic.ICollection`1<class Application.Post> V_1)
+            //IL_0000:  newobj instance void Application.BlogContext::.ctor()
+            //IL_0005:  stloc.0
+            //.try
+            //{
+            //  IL_0006:  ldloc.0
+            //  IL_0007:  ldnull
+            //  IL_0008:  callvirt instance class [mscorlib]
+            //      System.Collections.Generic.IEnumerable`1<!!0> [Mappy]
+            //      Mappy.DbContext::Repository<class Application.Post>(class [Mappy]
+            //      Mappy.Queries.SqlQuery`1<!!0>)
+            //  IL_000d:  call class [mscorlib]
+            //      System.Collections.Generic.List`1<!!0> [System.Core]
+            //      System.Linq.Enumerable::ToList<class Application.Post>(class [mscorlib]
+            //      System.Collections.Generic.IEnumerable`1<!!0>)
+            //  IL_0012:  stloc.1
+            //  IL_0013:  leave.s IL_001f
+            //}  // end .try
+            //finally
+            //{
+            //IL_0015:  ldloc.0
+            //IL_0016:  brfalse.s IL_001e
+            //IL_0018:  ldloc.0
+            //IL_0019:  callvirt instance void[mscorlib]
+            //System.IDisposable::Dispose()
+            //IL_001e:  endfinally
+            //}  // end handler
+            //IL_001f:  ldloc.1
+            //IL_0020:  ret
+            //} // end of method 
+
+            return getter;
         }
 
         private MethodBuilder DefineGetter(TypeBuilder typeBuilder, PropertyInfo propertyInfo, FieldBuilder field)
